dc1c3ef0a6fc48ad911f6bd201a8b901d3001537
Hasham Mubeen <68544555+hasham077@users.noreply.github.com>
2025-11-02 16:13:44 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'UnScreen Bg Remover',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: SplashScreen(),
     );
